d8eab507d48902995666f1030c8c74f9c3b12c91
Noel <noel.kghtet.99@gmail.com>
2026-03-23 11:27:49 +0700

    spawn timer

--- scripts/homes.gd
+++ scripts/homes.gd
@@ -1,14 +1,16 @@
 extends MeshInstance3D
 
 @onready var spawn_point = $spawnPoint
+@onready var spawn_timer = $spawn_timer
 @export var person_scene : PackedScene
 
 
 
 func _ready():
-	while true:
-		spawn_person()
-		await get_tree().create_timer(5).timeout
+	spawn_timer.wait_time = randf_range(1.0 , 6.0)
+	spawn_timer.start()
+	
+
 	
 func spawn_person():
 	var person = person_scene.instantiate()
@@ -16,3 +18,11 @@ func spawn_person():
 	get_parent().add_child.call_deferred(person)
 	person.global_position = spawn_point.global_position
 	person.home_position = spawn_point.global_position
+
+
+
+
+
+func _on_spawn_timer_timeout() -> void:
+	spawn_person()
+	spawn_timer.wait_time = randf_range(1.0 , 6.0)
