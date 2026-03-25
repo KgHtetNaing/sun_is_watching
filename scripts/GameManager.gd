0cb234128376eb0ad8291b57ca07e532b720b97c
Noel <noel.kghtet.99@gmail.com>
2026-03-25 15:24:52 +0700

    Lose screen and retry button

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -11,6 +11,7 @@ signal start_game_requested #to restart the timer when new game start
 
 func add_escape():
 	escapee += 1
-	if escapee >= 10:
+	if escapee >= 1:
 		print("Too many escape, you lose")
+		get_tree().change_scene_to_file("res://scenes/lose_screen.tscn")
 		
---
+++ scripts/lose_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+	$CenterContainer/VBoxContainer/HBoxContainer2/current_level.text = str(GameManager.current_level)
+
+
+func _on_retry_pressed() -> void:
+	GameManager.current_level = 1
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
+	
