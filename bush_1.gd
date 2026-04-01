6bec27bdc4bbac07b70bccb08f40b328ec8652bb
arnon-vongarnon <arnon.vongarnon@gmail.com>
2026-04-02 00:22:03 +0700

    Art-Map

---
+++ bush_1.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var sprite = $Sprite3D/AnimationPlayer
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite.play("DancingBush1")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
